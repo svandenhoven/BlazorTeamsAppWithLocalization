8801f1bd4495651180b82030a3e6998c2d8f91c7
Sander van den Hoven <sandervd@microsoft.com>
2022-06-17 15:58:17 +0200

    Code improvements

---
+++ BlazorTeamsApp/Components/ConfigBase.cs
@@ -0,0 +1,41 @@
+﻿using BlazorTeamsApp.Interop.TeamsSDK;
+using BlazorTeamsApp.Utils;
+using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Localization;
+
+namespace BlazorTeamsApp.Components
+{
+    public class ConfigBase : ComponentBase
+    {
+        [Inject] protected NavigationManager NavMan { get; set; }
+        [Inject] private MicrosoftTeams MicrosoftTeams { get; set; }
+        [Inject] protected IStringLocalizer<ConfigBase> Loc { get; set; }
+
+        private Guid _entityId = Guid.NewGuid();
+        protected string _buttonText;
+             
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+            _buttonText = Loc["Click"];
+        }
+
+        protected async Task ButtonClicked()
+        {
+            LocalizationUtil.SetCulture(NavMan);
+
+            _buttonText = Loc["Language"];
+
+            var settings = new TeamsInstanceSettings
+            {
+                SuggestedDisplayName = "My Tab",
+                EntityId = _entityId.ToString(),
+                ContentUrl = $"{NavMan.BaseUri}/tab",
+                WebsiteUrl = $"{NavMan.BaseUri}/tab"
+            };
+
+            await MicrosoftTeams.InitializeAsync();
+            await MicrosoftTeams.RegisterOnSaveHandlerAsync(settings);
+        }
+    }
+}
---
+++ BlazorTeamsApp/Utils/LocalizationUtil.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.Extensions.Localization;
+using System.Globalization; 
+
+namespace BlazorTeamsApp.Utils
+
+{
+    public static class LocalizationUtil
+    {
+        public static void SetCulture(NavigationManager navigationManager)
+        {
+            var uri = new Uri(navigationManager.Uri);
+            if (QueryHelpers.ParseQuery(uri.Query).TryGetValue("culture", out var culture))
+            {
+                if (!string.IsNullOrEmpty(culture))
+                {
+                    Thread.CurrentThread.CurrentCulture = new CultureInfo(culture.ToString());
+                    Thread.CurrentThread.CurrentUICulture = new CultureInfo(culture.ToString());
+                }
+            }
+        }
+    }
+}
